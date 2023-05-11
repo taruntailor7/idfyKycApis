c04a9c58d5f6dfb9c7cf0c7815baa0b1cdb2458f
Gurmeet Singh <gurmeet@crimecheck.in>
2023-05-12 00:49:51 +0530

    Added negative cases

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const db = require('./public/database');
+const request = require('request');
 const apiUrl = process.env.idfyApiUrl;
+
 // const verifyIfPanIsValidPanDummy=require('./DummyFiles/verifyIfPanIsValidPanDummy.json');
 // const getPanRequestOutput=require('./DummyFiles/PanVerifyResponse.json');
 const verifyIfPanIsValidPanDummy = require('./DummyFiles/verifyIfPanIsValidPanDummy.json');
@@ -27,13 +29,15 @@ app.get('/', (req, res) => {
     res.render('index', { title: 'My App', message: 'Welcome to my app!' });
 });
 
+const { API_KEY, ACCOUNT_ID, IDFY_API_URL } = process.env;
+
 app.get('/getUserInfo', async (req, res) => {
     const referenceId = req.query.referenceId;
     let response = await db.getUserDetails(referenceId);
     if (typeof response !== 'object') {
-        res.status(500).send('User information Added');
+        res.status(500).send({ response: false });
     } else {
-        res.status(200).send(response);
+        res.status(200).send({response: response});
     }
 });
 
@@ -42,24 +46,26 @@ app.post('/insertOrUpdateUserInfo', async (req, res) => {
     const data = JSON.parse(req.body.data);
     let response = await db.updateUserDetails(referenceId, data);
     if (response === 'Added') {
-        res.status(200).send('User information Added');
+        res.status(200).send({response: true});
     } else {
-        res.status(500).send(response);
+        res.status(500).send({response: false});
     }
 
 });
 
-app.post('/verifyPanDetails', async (req, res) => {
+app.post('/verifyIdDetails', async (req, res) => {
     const imageUrl = req.body.panImage;
     const referenceId = req.body.referenceId;
-    var request = require('request');
+    const typeOfDocument = req.body.typeOfDocument;
+    let responseForVlaidationPan = '';
+    // Comment 1 - This part contains extraction API call which we fetch extracted data for PAN.
     // var options = {
     //     'method': 'POST',
     //     'url': `${idfyApiUrl}/v3/tasks/async/extract/ind_pan`,
     //     'headers': {
     //         'Content-Type': 'application/json',
-    //         'account-id': process.env.ACCOUNT_ID,
-    //         'api-key': process.env.API_KEY
+    //         'account-id': ACCOUNT_ID,
+    //         'api-key': API_KEY
     //     },
     //     body: JSON.stringify({
     //         "task_id": "1",
@@ -75,7 +81,6 @@ app.post('/verifyPanDetails', async (req, res) => {
     //     if (error) return res.status(500).send({ error: err, message: err.message });
     //     requestIdForPanReq=res.body.request_id;
     // });
-    // var request = require('request');
     // var options = {
     //     'method': 'GET',
     //     'url': `${idfyApiUrl}/v3/tasks?request_id=${requestIdForPanReq}`,
@@ -89,41 +94,92 @@ app.post('/verifyPanDetails', async (req, res) => {
     //     if (error) throw new Error(error);
     //     console.log(response.body);
     // });
-    let requestIdForPanIsValidReq = verifyIfPanIsValidPanDummy;
-    let requestIdForPanIsValidReqGetStatus = verifyIfPanIsValidPanDummyGetStatus;
-    let requestIdForValidationPan = requestIdForPanIsValidReq['request_id'];
-    let responseForVlaidationPan = requestIdForPanIsValidReqGetStatus[requestIdForValidationPan];
-    console.log(responseForVlaidationPan);
-    if ('is_readable' in responseForVlaidationPan) {
-        if (!responseForVlaidationPan['is_readable']) {
-            res.status(200).send('Invalid Pan');
-        } else {
-            console.log('Content is readable');
-        }
-    } else {
-        res.status(501).send('Issue in verifing Pan');
-    }
-    if ('readability' in responseForVlaidationPan) {
-        if ('confidence' in responseForVlaidationPan['readability']) {
-            if (responseForVlaidationPan['readability']['confidence'] < 50) {
-                res.status(200).send('Invalid Pan');
+
+    // Comment 2 - This part contains dummy response of verifing documents
+    // let requestIdForPanIsValidReq = verifyIfPanIsValidPanDummy;
+    // let requestIdForPanIsValidReqGetStatus = verifyIfPanIsValidPanDummyGetStatus;
+    // let requestIdForValidationPan = requestIdForPanIsValidReq['request_id'];
+    // let responseForVlaidationPan = requestIdForPanIsValidReqGetStatus[requestIdForValidationPan];
+    // if ('is_readable' in responseForVlaidationPan) {
+    //     if (!responseForVlaidationPan['is_readable']) {
+    //         res.status(200).send('Invalid Pan');
+    //     } else {
+    //         console.log('Content is readable');
+    //     }
+    // } else {
+    //     res.status(501).send('Issue in verifing Pan');
+    // }
+
+    // Sending request to verify ID entered.
+    var options = {
+        'method': 'POST',
+        'url': `${IDFY_API_URL}/v3/tasks/async/validate/document`,
+        'headers': {
+            'Content-Type': 'application/json',
+            'account-id': ACCOUNT_ID,
+            'api-key': API_KEY
+        },
+        body: JSON.stringify({
+            "task_id": "74f4c926-250c-43ca-9c53-453e87ceacd1",
+            "group_id": "8e16424a-58fc-4ba4-ab20-5bc8e7c3c41e",
+            "data": {
+                "document1": imageUrl,
+                "doc_type": typeOfDocument,
+                "advanced_features": {
+                    "detect_doc_side": false
+                }
             }
-        } else {
-            res.status(501).send('Issue in verifing Pan');
-        }
-    } else {
-        res.status(501).send('Issue in verifing Pan');
-    }
-    let data = {
-        'pan':imageUrl
-    }
-    console.log(JSON.stringify(data,null,5)+ ' - data');
-    let response = await db.updateUserDetails(referenceId, data);
-    if (response === 'Added') {
-        res.status(200).send('User information Added');
-    } else {
-        res.status(500).send(response);
-    }
+        })
+
+    };
+    let requestId = '';
+    request(options, async function (error, response) {
+        if (error) throw new Error(error);
+        let body = JSON.parse(response.body);
+        console.log(body + ' - body');
+        requestId = body.request_id;
+        console.log(requestId + ' - requestId');
+        var options = {
+            'method': 'GET',
+            'url': `${IDFY_API_URL}/v3/tasks?request_id=${requestId}`,
+            'headers': {
+                'api-key': API_KEY,
+                'Content-Type': 'application/json',
+                'account-id': ACCOUNT_ID
+            }
+        };
+        setTimeout(function () {
+            // Your code here
+            request(options, async function (error, responseForCheckingPanResponse) {
+                if (error) throw new Error(error);
+                console.log(JSON.stringify(responseForCheckingPanResponse.body, null, 5));
+                responseForVlaidationPan = responseForCheckingPanResponse ? responseForCheckingPanResponse.body : null;
+                responseForVlaidationPan = JSON.parse(responseForVlaidationPan);
+                if (responseForVlaidationPan && responseForVlaidationPan[0].result) {
+                    if (responseForVlaidationPan[0].result.readability) {
+                        if (responseForVlaidationPan[0].result.readability.confidence < 50) {
+                            return res.status(200).send({ response: 'false' });
+                        }
+                    } else {
+                        return res.status(501).send({ response: 'false' });
+                    }
+                } else {
+                    return res.status(501).send({ response: 'false' });
+                }
+                let data = {
+                    'pan': imageUrl
+                }
+                console.log(JSON.stringify(data, null, 5) + ' - data');
+                let responseAfterInsertion = await db.updateUserDetails(referenceId, data);
+                if (responseAfterInsertion === 'Added') {
+                    return res.status(200).send({ response: 'true' });
+                } else {
+                    return res.status(500).send({ response: 'false' });
+                }
+            });
+        }, 5000);
+    });
+
 });
 
 // Start the server
